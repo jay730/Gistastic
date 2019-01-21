f7759ea6f5229d96afe3c68aa290d501fb598ba7
Jay Neupane <jayesh.neupane@gmail.com>
2019-01-21 12:07:56 -0600

    commit

--- giftastic.js
+++ giftastic.js
@@ -1,38 +1,76 @@
-var topics = ["Russia","USA","Canada", "China"];
-var deleteEnabled = false;
+$(document).ready(function(){
 
-    alert($(this).attr("data-name"));
-    
-    var country = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    country + "&api_key=dc6zaTOxFJmzC&limit=10";
+var countries = ["Russia","USA","Canada", "China"];
 
-    function makeApiCall() {
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-        }).then(function(response) {
-        console.log(response);
+            $("#buttons-view").empty();
+            
+            for(var i=0;i<countries.length;i++){
+                var btn = $("<button>");
+                btn.addClass("country-name");
+                btn.attr("data-name", countries[i]);
+                btn.text(countries[i]);
+                $("#buttons-view").append(btn);
+            }
+
+            $("#add-button").on("click", function(){
+                var c_value = $("#country-input").val();
+                $("#country-input").val("");
+
+                countries.push(c_value);
+                $("#buttons-view").empty();
+            
+            for(var i=0;i<countries.length;i++){
+                var btn = $("<button>");
+                btn.addClass("country-name");
+                btn.attr("data-name", countries[i]);
+                btn.text(countries[i]);
+                $("#buttons-view").append(btn);
+            }
+            });
+
+            $(document).on("click", "button", function(){
+                $("#gifArea").empty();
+
+                var country_temp = $(this).attr("data-name");
+
+                var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                country_temp + "&api_key=dc6zaTOxFJmzC&limit=10";
+        
+              
+              $.ajax({
+                url: queryURL,
+                method: "GET"
+              })
+                
+                .then(function(response) {
+                  console.log(queryURL);
+        
+                  console.log(response);
+                  var results = response.data;
+
+                  for(var i=0;i<results.length;i++){
+                        var countryDiv = $("<div>");
+                        var p = $("<p>").text("Ratings: "+results[i].rating);
+                        var countryGif = $("<img>");                     
+                        countryGif.attr("src", results[i].images.downsized_still.url);  
+                        countryGif.attr("data-still", results[i].images.downsized_still.url);                   
+                        countryGif.attr("data-animate", results[i].images.downsized.url);   
+                        countryGif.attr("data-state", "still"); 
+                        countryGif.addClass("gifStyles");                  
+                        countryDiv.append(p);
+                        countryDiv.append(countryGif);                        
+                        $("#gifArea").prepend(countryDiv);
+                    }
+            });
         });
 
-        //var gifDiv = $("<div class='gif'>");
-        //var rating = response.Rated;
-    }
-
-
-    // This function handles events where a movie button is clicked
-    $("#add-country").on("click", function(event) {
-    event.preventDefault();
-    // This line grabs the input from the textbox
-    var movie = $("#country-input").val().trim();
-    // Adding movie from the textbox to our array
-    countries.push(movie);
-    // Calling renderButtons which handles the processing of our movie array
-    renderButtons();
-    });
-    
-    // Adding a click event listener to all elements with a class of "movie-btn"
-    $(document).on("click", ".country-btn", makeApiCall);
-    // Calling the renderButtons function to display the intial buttons
-    renderButtons();
-;
+        $(document).on("click", ".gifStyles", function(){
+            var state = $(this).attr("data-state");
+            if(state === "animate"){
+                $(this).attr("src", $(this).data("still")); $(this).attr("data-state","still");
+            }
+            else{
+                $(this).attr("src", $(this).data("animate")); $(this).attr("data-state","animate");
+            }
+        })
+        })
